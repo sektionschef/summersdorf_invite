dba166f0cf99c6bd8251fcb7d8c659c8294122ea
Stefan Schwaha <the.stevee@gmail.com>
2021-01-24 20:32:25 +0100

    works with interactive angle

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Personal but public invites to go to Summersdorf.
 
 ## got source code from
 
-<https://editor.p5js.org/priyadarshan/sketches/lrthRa1m>
+- basic concept: <https://editor.p5js.org/priyadarshan/sketches/lrthRa1m>
+- interactive arc: <https://stackoverflow.com/questions/52484101/move-along-an-arc-in-p5js>
+
+tutorial github pages from: <https://www.youtube.com/watch?v=ZneWjyn18e8>
--- sketch.js
+++ sketch.js
@@ -1,40 +1,65 @@
-let red=0;
-let green=0;
-let blue=0;
-let sun=400;
-function setup(){
-  createCanvas(1000, 400);
+// mouse interactivity
+// blue sky
+// bird sound
+
+let canvasWidth = 1000;
+let canvasHeight = 800;
+
+let red = 0;
+let green = 0;
+let blue = 0;
+
+// start position sun
+let sunY = canvasHeight;
+
+
+let sunCenterX = canvasWidth / 2;
+let sunCenterY = canvasHeight / 2;
+let radius = 200;
+
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
 }
 
-function draw(){
-  background(red+=5, green++, blue);
+function draw() {
+  // color change
+  background(red += 5, green++, blue);
 
 
   //sun
   //outer
   noStroke();
   fill(255, 165, 0, 50);
-  if(sun>144){
-    circle(500, sun--, 200);
-  }
-  else if(sun==144){
-    circle(500, sun, 200);
+  if (sunY > 144) {
+    circle(500, sunY--, 200);
+  } else if (sunY == 144) {
+    circle(500, sunY, 200);
   }
   //inner
   noStroke();
   fill(255, 100, 0, 100);
-  if(sun>144){
-    circle(500, sun--, 150);
+  if (sunY > 144) {
+    circle(500, sunY--, 150);
+  } else if (sunY == 144) {
+    circle(500, sunY, 150);
   }
-  else if(sun==144){
-    circle(500, sun, 150);
-  }
+
+  var angle = map(mouseX, 0, width, PI, 2*PI);
+  let sunsX = sunCenterX + cos(angle) * radius;
+  let sunsY = sunCenterY + sin(angle) * radius;
+  circle(sunsX, sunsY, 100);
 
 
   noStroke();
   fill(150, 75, 0);
-  triangle(0, 400, 250, 200, 500, 400);
+  triangle(0, canvasHeight, 250, canvasHeight / 2, 500, canvasHeight);
   fill(150, 100, 0);
-  triangle(500, 400, 750, 200, 1000, 400);
+  // triangle(500, 400, 750, 200, 1000, 400);
+
+  textSize(26);
+  let s = 'Summersdorf Office';
+  // fill(50);
+  text(s, 100, 100, 300, 200); // Text wraps within text box
 
 }
