4532e36fad021f730b75cbb3d7f11d8a4df8a102
Stefan Schwaha <the.stevee@gmail.com>
2021-01-24 21:43:10 +0100

    color changes work

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Personal but public invites to go to Summersdorf.
 ## got source code from
 
 - basic concept: <https://editor.p5js.org/priyadarshan/sketches/lrthRa1m>
-- interactive arc: <https://stackoverflow.com/questions/52484101/move-along-an-arc-in-p5js>
+- interactive arc: <https://stackoverflow.com/questions/52484101/move-along-an-arc-in-p5js> and <https://editor.p5js.org/sektionschef/sketches/U9iE0d9rA>
 
 tutorial github pages from: <https://www.youtube.com/watch?v=ZneWjyn18e8>
--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ let blue = 0;
 
 // start position sun
 let sunY = canvasHeight;
+let sunMaxY = 144;
 let risen = false;
 
 
@@ -19,29 +20,52 @@ let sunCenterX = canvasWidth / 2;
 let sunCenterY = canvasHeight / 2;
 let radius = 200;
 
+let endBlue;
+let startYellow;
+let secondaryBlue;
+let colorNow;
+let amount;
+let black;
+
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
+
+  endBlue = color(112, 208, 255);
+  startYellow = color(255, 222, 112);
+  secondaryBlue = color(58, 110, 250);
+  black = color(0,0,0);
 }
 
 function draw() {
-  // color change
-  background(red += 5, green++, blue);
-
-
   // dynamics of sun
   var angle = map(mouseX, 0, width, PI, 2*PI);
   let sunDynX = sunCenterX + cos(angle) * radius;
   let sunDynY = sunCenterY + sin(angle) * radius;
 
+  // background(red += 5, green++, blue);
+
+  // color change
+  if (!risen && sunY >= (height/2)) {
+    amount = map(sunY, height, (height/2), 0, 1);
+    colorNow = lerpColor(black, startYellow, amount);
+  } else if (!risen && sunY < (height/2)) {
+    amount = map(sunY, height/2, sunMaxY, 0, 1);
+    colorNow = lerpColor(startYellow, endBlue, amount);
+  } else {
+    amount = map(sunDynY, (sunMaxY+400), sunMaxY, 0, 1);
+    colorNow = lerpColor(secondaryBlue, endBlue, amount);
+  }
+  background(colorNow);
+
 
   //sun
   //outer
   noStroke();
-  fill(255, 165, 0, 50);
-  if (sunY > 144 && !risen) {
+  fill(255, 165, 0, 100);
+  if (sunY > sunMaxY && !risen) {
     circle(500, sunY--, 200);
-  } else if (sunY == 144 && !risen) {
+  } else if (sunY == sunMaxY && !risen) {
     circle(500, sunY, 200);
     risen = true;
   } else {
@@ -49,10 +73,10 @@ function draw() {
   }
   //inner
   noStroke();
-  fill(255, 100, 0, 100);
-  if (sunY > 144 && !risen) {
+  fill(255, 100, 0, 150);
+  if (sunY > sunMaxY && !risen) {
     circle(500, sunY--, 150);
-  } else if (sunY == 144 && !risen) {
+  } else if (sunY == sunMaxY && !risen) {
     circle(500, sunY, 150);
     risen = true;
   } else {
@@ -62,7 +86,7 @@ function draw() {
   // mountain
   noStroke();
   fill(150, 75, 0);
-  triangle(0, canvasHeight, 250, canvasHeight / 2, 500, canvasHeight);
+  triangle(0, height, 250, height / 2, 500, height);
   fill(150, 100, 0);
 
 
